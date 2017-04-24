7e0a5cfad9a6f4cd81e30f870102517a4816c5ca
urban <urban@ds.pg.gda.pl>
2017-04-24 22:55:32 +0200

    Enabled scaling, added template scripts

--- templates/upload_shakespeare.sh
+++ templates/upload_shakespeare.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
+
+set -o errexit
+set -o nounset
+
+PORT=${1-9200}
+
 if [[ ! -f 'shakespeare.json' ]]; then
+    echo "downloading sample data..."
     wget https://download.elastic.co/demos/kibana/gettingstarted/shakespeare.json
 fi
 
-curl -H 'Content-Type: application/x-ndjson' -XPOST 'localhost:9200/shakespeare/_bulk?pretty' -u elastic:changeme --data-binary @shakespeare.json
+echo "Create mappings..."
+curl -H 'Content-Type: application/json' -XPUT http://localhost:${PORT}/shakespeare -d '
+{
+ "mappings" : {
+  "_default_" : {
+   "properties" : {
+    "speaker" : {"type": "string", "index" : "not_analyzed" },
+    "play_name" : {"type": "string", "index" : "not_analyzed" },
+    "line_id" : { "type" : "integer" },
+    "speech_number" : { "type" : "integer" }
+   }
+  }
+ }
+}
+';
+echo
+echo "Bulk uploading data..." 
+curl -H 'Content-Type: application/x-ndjson' -XPOST "localhost:${PORT}/shakespeare/_bulk?pretty" --data-binary @shakespeare.json
+
+echo "List of indices:"
+curl "localhost:${PORT}/_cat/indices?v"
+
